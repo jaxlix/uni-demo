4d1dc5b9039a82edcdb349358f1aa2683d897af9
jaxlix <jax.lix@qq.com>
2021-05-25 18:18:30 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		<navigator url="../list/list">列表</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: '/login'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
---
+++ pages/list/list.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<scroll-view scroll-y="true" >
+			<view v-for="(d, i) in list" :key="i">{{d.name}}</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'},
+					{name: '111'}
+				]
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
